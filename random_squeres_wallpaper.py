f90f249d2ab8820c5c68c8e62c3d80c92f5bebd2
Adrian <andarms@outlook.com>
2015-07-16 00:53:38 -0500

    Generate random height to rect in same column

--- random_squeres_wallpaper.py
+++ random_squeres_wallpaper.py
@@ -9,23 +9,24 @@ pg.init()
 SCREEN_WIDTH = pg.display.Info().current_w
 SCREEN_HEIGHT = pg.display.Info().current_h
 
-max_width = SCREEN_WIDTH/6
-max_height = SCREEN_HEIGHT/6
+max_size = 100
+min_size = 20
 
 image = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 width = SCREEN_HEIGHT
-height = SCREEN_HEIGHT
-x = 0
-y = 0
+
+x, y = 0, 0
 while x < SCREEN_WIDTH:
-	size = random.randint(0, max_width)
-	if size + x > SCREEN_WIDTH: size = SCREEN_WIDTH - x
-	for y in xrange(0, SCREEN_HEIGHT, size):
+	w = random.randint(min_size, max_size)
+	while y < SCREEN_HEIGHT:
+		h = random.randint(min_size, max_size)
 		color = [random.randint(0,255) for _ in range(3)]
-		rect = (x, y, size, size)
+		rect = (x, y, w, h)
 		image.fill(color, rect)
-	x += size
+		y += h
+	x += w
+	y = 0
 
 
 
