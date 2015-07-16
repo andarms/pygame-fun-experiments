08c025db5b3e0c58a2f7f14ceb18d0add3c209c6
Adrian <andarms@outlook.com>
2015-07-16 01:23:09 -0500

    Totally ramdon squares always into the canvas

--- random_squeres_wallpaper.py
+++ random_squeres_wallpaper.py
@@ -1,3 +1,7 @@
+"""
+inspired by https://github.com/anshulc95/rangeela
+Just a wallpaper generator to test pygame.image.save function 
+"""
 import random
 import sys
 
@@ -9,27 +13,31 @@ pg.init()
 SCREEN_WIDTH = pg.display.Info().current_w
 SCREEN_HEIGHT = pg.display.Info().current_h
 
-max_size = 100
-min_size = 20
+max_size = 200
+min_size = 50
+amount = 100
+bg_color = (0,0,0)
 
 image = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+image.fill(bg_color)
 
 width = SCREEN_HEIGHT
 
-x, y = 0, 0
-while x < SCREEN_WIDTH:
-	w = random.randint(min_size, max_size)
-	while y < SCREEN_HEIGHT:
-		h = random.randint(min_size, max_size)
-		color = [random.randint(0,255) for _ in range(3)]
-		rect = (x, y, w, h)
-		image.fill(color, rect)
-		y += h
-	x += w
-	y = 0
+print "Creating wallpaper wait please..."
+
+canvas_w = SCREEN_WIDTH - max_size
+canvas_h = SCREEN_HEIGHT - max_size
+for x in xrange(amount):
+	size = random.randint(min_size, max_size)
+	x = random.randint(0, canvas_w)
+	y = random.randint(0, canvas_h)
+	color = [random.randint(0,255) for _ in range(3)]		
+	rect = (x, y, size, size)	
+	image.fill(color, rect)
 
 
 
 pg.image.save(image, 'wallpaper.png')
+print "Done."
 pg.quit()
 sys.exit()
